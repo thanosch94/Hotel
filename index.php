bf631b3ce0e91f8b392b6455d363faf70a686c6d
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-03-15 23:54:05 +0200

    Add elements to  homepage

--- index.php
+++ index.php
@@ -7,10 +7,24 @@
     <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/022912981f.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header></header>
-    <main></main>
-    <footer></footer>
+    <header>
+        <nav class="d-flex justify-content-around pt-4 bg-light">
+            <p>Hotels</p>
+     
+            <div class="">
+                <i class="fa-solid fa-house"></i>
+                <p class="d-inline">Home</p>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <img class="img-fluid" src="images/main.jpg">
+    </main>
+    <footer>
+        <p class="text-center p-3 bg-light">© Copyright 2023 Hotels. All rights reserved.</p>
+    </footer>
 </body>
 </html>
