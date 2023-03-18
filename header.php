fda5e4a72e3e74c671689d042d4782d527b6b42b
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-03-18 21:20:26 +0200

    Create new file for header

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+        <nav class="d-flex justify-content-around pt-4 bg-light">
+            <p>Hotels</p>
+     
+            <div>
+                <a href="index.php" class="text-decoration-none text-body"><i class="fa-solid fa-house"></i>
+                <p class="d-inline pe-3">Home</p></a>
+                <a href="login.php" class="text-decoration-none text-body"><i class="fa-solid fa-user"></i>
+                <p class="d-inline">Profile</p></a>
+            </div>
+            
+        </nav>
+    </header>
