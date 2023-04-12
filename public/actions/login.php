ee65b442307b12947fa800f31fb15e12e067d3ce
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-04-12 17:51:31 +0300

    Login actions when a user clicks login button

---
+++ public/actions/login.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__.'\..\..\boot\boot.php';
+
+use Hotel\User;
+
+//Return to login page if not a post request
+if(strtolower($_SERVER['REQUEST_METHOD']) != 'post'){
+    header('Location: /hotel/public/login.php');
+    return;
+}
+
+//Create new user
+$user = new User;
+
+
+//Verify user info
+$verify= $user->verify($_REQUEST['email'], $_POST['password']);
+if($verify){
+    header('Location: /hotel/public/index.php');
+}
+
+//Retrieve user
+$userInfo = $user->getByEmail($_REQUEST['email']);
+
+//Generate token
+$token =$user->generateToken($userInfo['user_id']);
+
+//Set cookie
+setcookie('user_id', $token, time()+(30*24*60*60), '/');
+
+
+?>
