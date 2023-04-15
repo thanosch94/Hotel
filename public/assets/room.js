b07978ce84eea2da6f3580519f47888839a81618
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-04-15 17:36:45 +0300

    Autocomplete checkin and checkout in room page

---
+++ public/assets/room.js
@@ -0,0 +1,6 @@
+//Autocomplete Check-in and Check-out dates in room page
+const checkInDate = document.querySelector(".checkInDate");
+const checkOutDate = document.querySelector(".checkOutDate");
+
+checkInDate.value = checkin;
+checkOutDate.value = checkout;
