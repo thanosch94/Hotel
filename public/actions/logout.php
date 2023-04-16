04910cab6641a4875f2b42106037143006245f4c
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-04-16 12:23:53 +0300

    Add logout by unsetting cookie

---
+++ public/actions/logout.php
@@ -0,0 +1,8 @@
+
+<?php 
+//Unset cookie by setting expiration time in the past
+setcookie("user_id", "", -time()+(30*24*60*60), "/");
+
+//Once logout head to Login page
+header('Location: /hotel/public/login.php');
+?>
