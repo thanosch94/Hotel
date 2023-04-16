d65dd7d27378ac49703dba164319a4349deed8b4
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-04-16 21:59:35 +0300

    Create newReview() to add a new review in a room

---
+++ app/Hotel/Review.php
@@ -0,0 +1,28 @@
+<?php
+namespace Hotel;
+use PDO;
+
+class Review{
+    private $pdo;
+
+    public function __construct(){
+        $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=hotel;charset=UTF8', 'thanosch', '123456789!', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"]);
+        
+    }
+    private function getPdo() {
+        return $this->pdo;
+    }
+    public function newReview($stars, $newReview){
+        $statement = $this->getPdo()->prepare('INSERT INTO review(room_id, user_id, rate, comment) VALUES (:room_id, :user_id, :rate, :comment)');
+        $room_id = $_POST['roomid'];
+        $user_id = $_POST['user'];
+        $statement ->bindParam(':room_id', $room_id, PDO::PARAM_INT);
+        $statement ->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $statement ->bindParam(':rate', $stars, PDO::PARAM_INT);
+        $statement ->bindParam(':comment', $newReview, PDO::PARAM_STR);
+
+        $statement->execute();
+
+
+    }
+}
